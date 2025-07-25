5fff6901cc40b47f98c2b98260b697afce125e08
SylvesterFraide <fridaysylvester2@gmail.com>
2025-07-25 18:55:45 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="app">
+        My portfolio website
+      </div>
+    </>
+  )
+}
+
+export default App
