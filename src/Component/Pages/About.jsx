87173543dfeeffbe2c0ae7724ca3bba6711f07b0
SylvesterFraide <fridaysylvester2@gmail.com>
2025-08-22 23:12:34 +0300

    7th commit

--- src/Component/Pages/About.jsx
+++ src/Component/Pages/About.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React from "react";
+import "./About.css";
+import web from "../../assets/web.png";
+import app from "../../assets/app.png";
+import UIUX from "../../assets/UIUX.png";
 
-export const About = () => {
+export const About = ({ skillTitle, skillsDesc, h1, p1, h2, p2, h3, p3 }) => {
   return (
-    <div>About</div>
+    <section id="skills">
+      <span className="skillTitle">{skillTitle}</span>
+      <span className="skillsDesc">{skillsDesc}</span>
+      <div className="skillBars">
+        <div className="skillBar">
+          <img src={UIUX} alt="" className="skillBarImg" />
+          <div className="skillBarText">
+            <h2>{h1}</h2>
+            <p>{p1}</p>
+          </div>
+        </div>
+        <div className="skillBar">
+          <img src={web} alt="" className="skillBarImg" />
+          <div className="skillBarText">
+            <h2>{h2}</h2>
+            <p>{p2}</p>
+          </div>
+        </div>
+        <div className="skillBar">
+          <img src={app} alt="" className="skillBarImg" />
+          <div className="skillBarText">
+            <h2>{h3}</h2>
+            <p>{p3}</p>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
--- src/Component/Pages/Home.jsx
+++ src/Component/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.css";
 import { Hero } from "../Hero/Hero";
+import { About } from './About'
 
 export const Home = () => {
   return (
@@ -18,6 +19,17 @@ export const Home = () => {
           </>
         }
         hire="Hire Me"
+      />
+
+      <About 
+        skillTitle="What I do"
+        skillsDesc="I am a skilled and passionate developer with expertise in web and mobile application development."
+        h1="UI/UX Design"
+        p1="Creating user-friendly interfaces and experiences."
+        h2="Web Development"
+        p2="Building responsive and interactive websites."
+        h3="App Development"
+        p3="Creating mobile applications for iOS and Android."
       />
     </div>
   );
