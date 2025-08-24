0fa9f888901446eab66e05d66b131c6ded89c23b
SylvesterFraide <fridaysylvester2@gmail.com>
2025-08-24 22:38:32 +0300

    12th commit

--- src/Component/Pages/Contact.jsx
+++ src/Component/Pages/Contact.jsx
@@ -4,6 +4,10 @@ import sha from "../../assets/Sha.jpeg";
 import microsoft from "../../assets/Microsoft.avif";
 import adobe from "../../assets/Adobe.webp";
 import facebook from "../../assets/fb.png";
+import fb1 from "../../assets/fb1.png";
+import ig from "../../assets/ig.png";
+import x from "../../assets/x.avif";
+import youtube from "../../assets/yuotube.webp";
 
 export const Contact = () => {
   return (
@@ -23,6 +27,29 @@ export const Contact = () => {
         </div>
       </div>
       <div id="contact">
+        <h1 className="contactPageItem">Contact Me</h1>
+        <span className="contactDesc">
+          Please fill in the form below to get in touch with me.
+        </span>
+        <form className="contactForm">
+          <input type="text" className="input" placeholder="Your Name" />
+          <input type="email" className="input" placeholder="Your Email" />
+          <textarea
+            className="input"
+            name="message"
+            rows="5"
+            placeholder="Your Message"
+          ></textarea>
+          <button type="submit" value="send" className="sbtn">
+            Submit
+          </button>
+          <div className="links">
+            <img src={fb1} alt="fb" className="link" />
+            <img src={ig} alt="ig" className="link" />
+            <img src={x} alt="x" className="link" />
+            <img src={youtube} alt="youtube" className="link" />
+          </div>
+        </form>
       </div>
     </section>
   );
